8e9b6d28eb304bd0437452b9b7375c3ebce9b0a7
SpaghettiCook <btheerebout@gmail.com>
2015-01-26 23:44:52 +0100

    halfway tutorial 2

--- app/src/main/java/brambv/myfirstapp/MyActivity.java
+++ app/src/main/java/brambv/myfirstapp/MyActivity.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-
+// some extra comments
 public class MyActivity extends ActionBarActivity implements View.OnClickListener{
 
     ArrayAdapter mArrayAdapter;
