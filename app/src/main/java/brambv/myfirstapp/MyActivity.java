5e1a544f38828fd8f303efdc5040155d18b315ca
SpaghettiCook <btheerebout@gmail.com>
2015-02-07 22:14:35 +0100

    bring on part 3!

--- app/src/main/java/brambv/myfirstapp/MyActivity.java
+++ app/src/main/java/brambv/myfirstapp/MyActivity.java
@@ -1,9 +1,13 @@
 package brambv.myfirstapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,13 +17,16 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.support.v7.widget.ShareActionProvider;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 // some extra comments, so
 public class MyActivity extends ActionBarActivity implements View.OnClickListener, AdapterView.OnItemClickListener{
+
+    private static final String PREFS = "prefs";
+    private static final String PREF_NAME = "name";
 
     ArrayAdapter mArrayAdapter;
     ArrayList mNameList = new ArrayList();
@@ -27,6 +34,7 @@ public class MyActivity extends ActionBarActivity implements View.OnClickListene
     EditText mMainEditText;
     ListView mMainListView;
     ShareActionProvider mShareActionProvider;
+    SharedPreferences mSharedPreferences;
     TextView mMainTextView;
 
     @Override
@@ -49,6 +57,9 @@ public class MyActivity extends ActionBarActivity implements View.OnClickListene
 
         // Set the ListView to use the ArrayAdapter
         mMainListView.setAdapter(mArrayAdapter);
+
+        // 7. Greet the user, or ask for their name if new
+        displayWelcome();
     }
 
     @Override
@@ -105,6 +116,59 @@ public class MyActivity extends ActionBarActivity implements View.OnClickListene
             // Make sure the provider knows
             // it should work with that Intent
             mShareActionProvider.setShareIntent(shareIntent);
+        }
+    }
+
+    public void displayWelcome() {
+
+        // Access the device's key-value storage
+        mSharedPreferences = getSharedPreferences(PREFS, MODE_PRIVATE);
+
+        // Read the user's name,
+        // or an empty string if nothing found
+        String name = mSharedPreferences.getString(PREF_NAME, "");
+
+        if (name.length() > 0) {
+
+            // If the name is valid, display a Toast welcoming them
+            Toast.makeText(this, "Welcome back, " + name + "!", Toast.LENGTH_LONG).show();
+        } else {
+
+            // otherwise, show a dialog to ask for their name
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Hello!");
+            alert.setMessage("What is your name?");
+
+            // Create EditText for entry
+            final EditText input = new EditText(this);
+            alert.setView(input);
+
+            // Make an "OK" button to save the name
+            alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface dialog, int whichButton) {
+
+                    // Grab the EditText's input
+                    String inputName = input.getText().toString();
+
+                    // Put it into memory (don't forget to commit!)
+                    SharedPreferences.Editor e = mSharedPreferences.edit();
+                    e.putString(PREF_NAME, inputName);
+                    e.commit();
+
+                    // Welcome the new user
+                    Toast.makeText(getApplicationContext(), "Welcome, " + inputName + "!", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            // Make a "Cancel" button
+            // that simply dismisses the alert
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface dialog, int whichButton) {}
+            });
+
+            alert.show();
         }
     }
 }
